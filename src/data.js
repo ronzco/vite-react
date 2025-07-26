a7bc2a4e2e35f74971212dd9c0040f4263c47b6b
RoniKoswara <ronisebastian974@gmail.com>
2025-07-26 20:19:33 +0700

    update halaman

---
+++ src/data.js
@@ -0,0 +1,156 @@
+import HeroImage from "/assets/ronker.png";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/reactjs.png";
+import Tools3 from "/assets/tools/codeigniter.svg";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools9 from "/assets/tools/ai.png";
+import Tools10 from "/assets/tools/canva.png";
+import Tools11 from "/assets/tools/figma.png";
+import Tools12 from "/assets/tools/xampp.webp";
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "200",
+  },
+  {
+    id: 3,
+    gambar: Tools3,
+    nama: "CodeIgniter",
+    ket: "Framework",
+    dad: "300",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 9,
+    gambar: Tools9,
+    nama: "Adobe Illustrator",
+    ket: "Design App",
+    dad: "900",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Figma",
+    ket: "Design App",
+    dad: "1100",
+  },
+  {
+    id: 12,
+    gambar: Tools12,
+    nama: "Xampp",
+    ket: "Database Management",
+    dad: "1100",
+  },
+];
+
+import Proyek1 from "/assets/proyek/project1.webp";
+import Proyek2 from "/assets/proyek/project2.webp";
+import Proyek3 from "/assets/proyek/project3.webp";
+import Proyek4 from "/assets/proyek/project4.webp";
+import Proyek5 from "/assets/proyek/project5.webp";
+
+export const listProyek = [
+  {
+    id: 1,
+    gambar: Proyek1,
+    nama: "Aplikasi Kalkulator WEB",
+    desk: "Tugas Kuliah Membangun Aplikasi WEB Calculator",
+    tools: ["HTML", "CSS", "Javascript"],
+    dad: "200",
+  },
+  {
+    id: 2,
+    gambar: Proyek2,
+    nama: "GIS Pertanian",
+    desk: "Project Tugas Akhir Kuliah masih dalam tahap Pengembangan",
+    tools: ["HTML", "CSS", "AdminLTE", "PHP", "MySql", "Codeigniter"],
+    dad: "300",
+  },
+  {
+    id: 3,
+    gambar: Proyek3,
+    nama: "Web Landing Page Toko Online",
+    desk: "Tugas Kuliah Ujian Akhir Semester 5 Mata Kuliah WEB Programming",
+    tools: ["HTML", "JavaScript", "CSS"],
+    dad: "400",
+  },
+  {
+    id: 4,
+    gambar: Proyek4,
+    nama: "Split-Bill Aplications",
+    desk: "Proyek Akhir Pelatihan Di CODEPOLITAN Membangun aplikasi spit bill berbasis web",
+    tools: ["Vite", "ReactJS", "Bootstrap", "JavaScript"],
+    dad: "500",
+  },
+  {
+    id: 5,
+    gambar: Proyek5,
+    nama: "WEB Catatan Harian",
+    desk: "Tugas Kuliah Membangun Aplikasi WEB Catatan Harian",
+    tools: ["HTML", "CSS", "Javascript", "Bootsrap"],
+    dad: "600",
+  },
+];
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "animate.css";
 
-createRoot(document.getElementById('root')!).render(
+import Navbar from "./components/Navbar.tsx";
+import Footer from "./components/Footer.tsx";
+import "remixicon/fonts/remixicon.css";
+import Preloader from "./components/Preloader.tsx";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Preloader />
+    <div className="container mx-auto px-4">
+      <Navbar />
+      <App />
+      <Footer />
+    </div>
+  </StrictMode>
+);
